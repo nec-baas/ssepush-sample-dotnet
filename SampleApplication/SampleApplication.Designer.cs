a370008ad1ae233a72674db58e1ee8f7dcd1192b
y-okamoto <y-okamoto@nh.jp.nec.com>
2018-11-30 17:39:53 +0900

    commit v7.0

---
+++ SampleApplication/SampleApplication.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace SampleApplication
+{
+    partial class SampleApplication
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LogTextBox = new System.Windows.Forms.TextBox();
+            this.SendMessageButton = new System.Windows.Forms.Button();
+            this.ClearLogButton = new System.Windows.Forms.Button();
+            this.DisconnectButton = new System.Windows.Forms.Button();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LogTextBox
+            // 
+            this.LogTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LogTextBox.Location = new System.Drawing.Point(0, 38);
+            this.LogTextBox.Multiline = true;
+            this.LogTextBox.Name = "LogTextBox";
+            this.LogTextBox.ReadOnly = true;
+            this.LogTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.LogTextBox.Size = new System.Drawing.Size(493, 255);
+            this.LogTextBox.TabIndex = 0;
+            this.LogTextBox.TabStop = false;
+            // 
+            // SendMessageButton
+            // 
+            this.SendMessageButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SendMessageButton.Location = new System.Drawing.Point(324, 2);
+            this.SendMessageButton.Name = "SendMessageButton";
+            this.SendMessageButton.Size = new System.Drawing.Size(150, 30);
+            this.SendMessageButton.TabIndex = 3;
+            this.SendMessageButton.Text = "Push送信";
+            this.SendMessageButton.UseVisualStyleBackColor = true;
+            this.SendMessageButton.Click += new System.EventHandler(this.SendMessageButton_Click);
+            // 
+            // ClearLogButton
+            // 
+            this.ClearLogButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ClearLogButton.Location = new System.Drawing.Point(343, 299);
+            this.ClearLogButton.Name = "ClearLogButton";
+            this.ClearLogButton.Size = new System.Drawing.Size(150, 30);
+            this.ClearLogButton.TabIndex = 4;
+            this.ClearLogButton.Text = "ログクリア";
+            this.ClearLogButton.UseVisualStyleBackColor = true;
+            this.ClearLogButton.Click += new System.EventHandler(this.ClearLogButton_Click);
+            // 
+            // DisconnectButton
+            // 
+            this.DisconnectButton.Location = new System.Drawing.Point(168, 2);
+            this.DisconnectButton.Name = "DisconnectButton";
+            this.DisconnectButton.Size = new System.Drawing.Size(150, 30);
+            this.DisconnectButton.TabIndex = 2;
+            this.DisconnectButton.Text = "切断";
+            this.DisconnectButton.UseVisualStyleBackColor = true;
+            this.DisconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(12, 2);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(150, 30);
+            this.ConnectButton.TabIndex = 1;
+            this.ConnectButton.Text = "接続";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // SampleApplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 331);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.DisconnectButton);
+            this.Controls.Add(this.ClearLogButton);
+            this.Controls.Add(this.SendMessageButton);
+            this.Controls.Add(this.LogTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "SampleApplication";
+            this.Text = "SampleApplication";
+            this.Load += new System.EventHandler(this.SampleApplication_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LogTextBox;
+        private System.Windows.Forms.Button SendMessageButton;
+        private System.Windows.Forms.Button ClearLogButton;
+        private System.Windows.Forms.Button DisconnectButton;
+        private System.Windows.Forms.Button ConnectButton;
+    }
+}
+
---
+++ SampleApplication/SampleApplication.cs
@@ -0,0 +1,170 @@
+﻿using Nec.Nebula;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleApplication
+{
+    public partial class SampleApplication : Form
+    {
+        // Nebulaサーバ テナント情報
+        public const string NebulaEndpointUrl = "";
+        public const string TenantId = "";
+        public const string AppId = "";
+        public const string AppKey = "";
+        
+        // Pushクライアント
+        NbSsePushReceiveClient client;
+
+        delegate void SetTextCallback(string text);
+
+        public SampleApplication()
+        {
+            InitializeComponent();
+        }
+
+        // アプリケーション実行時の処理
+        private async void SampleApplication_Load(object sender, EventArgs e)
+        {
+            // Nebula 初期化
+            InitNebula();
+
+            // インスタレーションを登録する
+            await RegisterInstallation();
+
+            // コールバックを登録する
+            RegisterCallback();
+        }
+
+        // Nebula 初期化
+        private void InitNebula()
+        {
+            // Nebula の生成
+            var service = NbService.GetInstance();
+
+            // テナントID
+            service.TenantId = TenantId;
+
+            // アプリケーションID
+            service.AppId = AppId;
+
+            // アプリケーションキー
+            service.AppKey = AppKey;
+
+            // エンドポイントURI
+            service.EndpointUrl = NebulaEndpointUrl;
+        }
+
+        // インスタレーションを登録する
+        private async Task<NbSsePushInstallation> RegisterInstallation()
+        {
+            // インスタレーションを取得する
+            var installation = NbSsePushInstallation.GetCurrentInstallation();
+
+            // 必須プロパティを設定する
+            installation.Channels = new HashSet<string> { "NewsChannel", "AlarmChannel" };
+            installation.AllowedSenders = new HashSet<string> { "g:anonymous" };
+
+            // オプションを設定する
+            NbJsonObject options = new NbJsonObject();
+            options.Add("email", "SseSampleApp@pushtest.com");
+            installation.Options = options;
+
+            // インスタレーションをサーバに登録する
+            return await installation.Save();
+        }
+
+        // コールバックを登録する
+        private void RegisterCallback()
+        {
+            // NbSsePushReceiveClient を生成
+            client = new NbSsePushReceiveClient();
+
+            // メッセージ受信コールバック
+            client.RegisterOnMessage("message", msg =>
+            {
+                // msg.Data に受信メッセージ(文字列)が引き渡される
+                SetText("Message: " + msg.Data);
+            });
+
+            // エラーコールバック
+            client.RegisterOnError((statusCode, errorInfo) =>
+            {
+                // エラー処理を行う
+                SetText("Error: " + statusCode.ToString() + ", " + errorInfo.ToString());
+            });
+
+            // 接続完了時コールバック
+            client.RegisterOnOpen(() =>
+            {
+                SetText("Open");
+            });
+
+            // 切断時コールバック
+            client.RegisterOnClose(() =>
+            {
+                SetText("Close");
+            });
+        }
+
+        // 接続ボタン押下時
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            client.Connect();
+        }
+
+        // 切断ボタン押下時
+        private void DisconnectButton_Click(object sender, EventArgs e)
+        {
+            client.Disconnect();
+        }
+
+        // Push送信ボタン押下時
+        private async void SendMessageButton_Click(object sender, EventArgs e)
+        {
+            // Push送信
+            await SendPush();
+        }
+
+        // Push送信
+        private async Task<NbJsonObject> SendPush()
+        {
+            var push = new NbPush();
+
+            // 宛先を作成する
+            var query = new NbQuery();
+            query.EqualTo("_channels", "AlarmChannel");
+            query.EqualTo("email", "SseSampleApp@pushtest.com");
+
+            // 宛先をセットする
+            push.Query = query;
+
+            // メッセージをセットする
+            push.Message = "Low battery.";
+
+            // Pushを送信する
+            return await push.SendAsync();
+        }
+
+        // ログクリアボタン押下時
+        private void ClearLogButton_Click(object sender, EventArgs e)
+        {
+            LogTextBox.Clear();
+        }
+
+        // ログ表示
+        private void SetText(string text)
+        {
+            if (this.LogTextBox.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.LogTextBox.AppendText(text + "\n");
+            }
+        }
+    }
+}
